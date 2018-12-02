393e0cb23e9c033be14db7e64c1f721c6ee68889
Owen Searfoss <owen@datura.io>
2018-12-02 01:57:23 -0600

    Checkpoint

--- src/main/java/io/datura/java/quizzes/advent2018/day02/AdventDay02.java
+++ src/main/java/io/datura/java/quizzes/advent2018/day02/AdventDay02.java
@@ -1,6 +1,9 @@
 package io.datura.java.quizzes.advent2018.day02;
 
+import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class AdventDay02 {
@@ -10,18 +13,67 @@ public class AdventDay02 {
 	private static String[] boxCodes = { "abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab" };
 
 	public static void main(String[] args) {
-		int[] result = countOccurrence("aabbcddeeefz");
-		System.out.println(printCharCountArray(result));
+		Map<Character, Integer> map = getCharacterMap("aabbcddeeefz");
+		System.out.println(map);
+	}
+
+	public static SimpleImmutableEntry<Integer, Integer> getDubsTripsTotal(
+			Collection<SimpleImmutableEntry<Boolean, Boolean>> l) {
+		int doubleCount = 0;
+		int tripleCount = 0;
+
+		for (SimpleImmutableEntry<Boolean, Boolean> lv : l) {
+			if (lv.getKey())
+				doubleCount++;
+
+			if (lv.getValue())
+				tripleCount++;
+		}
+
+		// the requirements don't specify what happens if the input has zero of either number
+		
+		return new SimpleImmutableEntry<Integer, Integer>(doubleCount, tripleCount);
+	}
+
+	// using this entry class as an ersatz Pair to avoid importing Commons
+	public static SimpleImmutableEntry<Boolean, Boolean> hasDubsTrips(Map<Character, Integer> input) {
+		Boolean hasDoubles = Boolean.FALSE;
+		Boolean hasTriples = Boolean.FALSE;
+
+		for (Integer v : input.values()) {
+			if (!hasDoubles && Integer.valueOf(2).equals(v))
+				hasDoubles = Boolean.TRUE;
+			else if (!hasTriples && Integer.valueOf(3).equals(v))
+				hasTriples = Boolean.TRUE;
+		}
+
+		return new SimpleImmutableEntry<Boolean, Boolean>(hasDoubles, hasTriples);
 	}
 
 	public static Map<Character, Integer> getCharacterMap(String boxCode) {
-		return Collections.emptyMap();
+		if (isEmpty(boxCode))
+			return Collections.emptyMap();
+
+		Map<Character, Integer> characterMap = new HashMap<>();
+
+		for (int i = 0, sz = boxCode.length(); i < sz; i++) {
+			char c = boxCode.charAt(i);
+
+			if (characterMap.containsKey(c)) {
+				Integer cv = characterMap.get(c);
+				characterMap.put(c, ++cv);
+			} else {
+				characterMap.put(c, 1);
+			}
+		}
+
+		return characterMap;
 	}
 
 	public static boolean isEmpty(String input) {
 		return input == null || input.isBlank();
 	}
-	
+
 	private static int[] countOccurrence(String vals) {
 		int[] counts = new int[ASCII_NUM_LOWERCASE];
 		for (char c : vals.toCharArray()) {
--- src/test/java/io/datura/java/quizzes/advent2018/AdventDay02Test.java
+++ src/test/java/io/datura/java/quizzes/advent2018/AdventDay02Test.java
@@ -3,6 +3,10 @@ package io.datura.java.quizzes.advent2018;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 import io.datura.java.quizzes.advent2018.day02.AdventDay02;
@@ -13,16 +17,62 @@ public class AdventDay02Test {
 		String testString = "somecontents here are some spaces hello";
 		assertFalse(AdventDay02.isEmpty(testString));
 	}
-	
+
 	@Test
 	public void testIsEmpty() {
 		String testString = "";
 		assertTrue(AdventDay02.isEmpty(testString));
 	}
-	
+
 	@Test
 	public void testNull() {
 		String testString = null;
 		assertTrue(AdventDay02.isEmpty(testString));
+	}
+
+	@Test
+	public void testDoubles() {
+		Map<Character, Integer> counts = new HashMap<>();
+		counts.put('a', 1);
+		counts.put('b', 2);
+		counts.put('g', 3);
+		counts.put('h', 2);
+		SimpleImmutableEntry<Boolean, Boolean> checkResult = AdventDay02.hasDubsTrips(counts);
+		assertTrue(checkResult.getKey());
+	}
+
+	@Test
+	public void testNoDoubles() {
+		Map<Character, Integer> counts = new HashMap<>();
+		counts.put('a', 1);
+		counts.put('b', 3);
+		counts.put('g', 5);
+		counts.put('h', 8);
+		counts.put('j', 4);
+		SimpleImmutableEntry<Boolean, Boolean> checkResult = AdventDay02.hasDubsTrips(counts);
+		assertFalse(checkResult.getKey());
+	}
+	
+	@Test
+	public void testTriples() {
+		Map<Character, Integer> counts = new HashMap<>();
+		counts.put('a', 1);
+		counts.put('b', 2);
+		counts.put('g', 3);
+		counts.put('h', 2);
+		SimpleImmutableEntry<Boolean, Boolean> checkResult = AdventDay02.hasDubsTrips(counts);
+		assertTrue(checkResult.getValue());
+	}
+
+	@Test
+	public void testNoTriples() {
+		Map<Character, Integer> counts = new HashMap<>();
+		counts.put('a', 1);
+		counts.put('b', 2);
+		counts.put('g', 5);
+		counts.put('h', 8);
+		counts.put('j', 4);
+		SimpleImmutableEntry<Boolean, Boolean> checkResult = AdventDay02.hasDubsTrips(counts);
+		assertFalse(checkResult.getValue());
 	}
 }
