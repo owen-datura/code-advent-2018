908069449f1d5b8ff1416f3dc4f3097939ae98bb
Owen Searfoss <owen@datura.io>
2018-12-02 23:21:22 -0600

    Start Day 03

---
+++ src/main/java/io/datura/java/quizzes/advent2018/day03/AdventDay03.java
@@ -0,0 +1,5 @@
+package io.datura.java.quizzes.advent2018.day03;
+
+public class AdventDay03 {
+
+}
---
+++ src/test/java/io/datura/java/quizzes/advent2018/AdventDay03Test.java
@@ -0,0 +1,5 @@
+package io.datura.java.quizzes.advent2018;
+
+public class AdventDay03Test {
+
+}
