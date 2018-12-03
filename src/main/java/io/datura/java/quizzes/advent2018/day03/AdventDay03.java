d8e54b93c775572bee0e5df4b06262baa089f9f4
Owen Searfoss <owen@datura.io>
2018-12-03 04:48:02 -0600

    Load and process the input.

--- src/main/java/io/datura/java/quizzes/advent2018/day03/AdventDay03.java
+++ src/main/java/io/datura/java/quizzes/advent2018/day03/AdventDay03.java
@@ -1,17 +1,41 @@
 package io.datura.java.quizzes.advent2018.day03;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import io.datura.java.quizzes.advent2018.day01.AdventDay01;
+
 public class AdventDay03 {
-	// private static int FABRIC_LEN = 1000;
-	private static int FABRIC_LEN = 10;
+	private static int FABRIC_LEN = 1000;
 
 	public static final char USAGE_NORMAL = 'o';
 	public static final char USAGE_CONFLICT = 'X';
-	
+
 	public static void main(String[] args) {
-		// it's specifically stated to be a square, so
-		// the l & w are equal
-		char[][] sheet = new char[FABRIC_LEN][FABRIC_LEN];
-		printFabricLayout(sheet);
+		try {
+			// load the claims from the provided data file
+			Collection<FabricClaim> claims = parseFabricClaims(getDataFromInputFile());
+			// it's specifically stated to be a square, so
+			// the l & w are equal
+			char[][] sheet = new char[FABRIC_LEN][FABRIC_LEN];
+			for (FabricClaim claim : claims) {
+				markClaim(sheet, claim);
+			}
+
+			printFabricLayout(sheet);
+
+			int conflicts = getFabricConflicts(sheet);
+			String output = String.format("Calculation of fabric claims complete. There were %d conflicts.", conflicts);
+			System.out.println(output);
+		} catch (IOException ioe) {
+			System.err.println("Encountered an I/O exception when processing input. Please check configuration.");
+			System.exit(1);
+		}
 	}
 
 	public static void markClaim(char[][] sheet, FabricClaim claim) {
@@ -22,11 +46,11 @@ public class AdventDay03 {
 		final int maxCol = initialCol + claim.getWidth();
 		// similarly, we need to determine the max row so we can iterate
 		final int maxRow = claim.getTopMargin() + claim.getHeight();
-		
+
 		for (int rowOffset = claim.getTopMargin(); rowOffset < maxRow; rowOffset++) {
 			for (int colOffset = initialCol; colOffset < maxCol; colOffset++) {
 				char curVal = sheet[rowOffset][colOffset];
-				if( curVal == 0 )
+				if (curVal == 0)
 					sheet[rowOffset][colOffset] = USAGE_NORMAL;
 				else
 					sheet[rowOffset][colOffset] = USAGE_CONFLICT;
@@ -39,11 +63,11 @@ public class AdventDay03 {
 		for (int i = 0, rows = sheet.length; i < rows; i++) {
 			for (int j = 0, cols = sheet[i].length; j < cols; j++) {
 				char val = sheet[i][j];
-				if( val != 0)
+				if (val != 0)
 					populatedCells++;
 			}
 		}
-		
+
 		return populatedCells;
 	}
 
@@ -52,14 +76,14 @@ public class AdventDay03 {
 		for (int i = 0, rows = sheet.length; i < rows; i++) {
 			for (int j = 0, cols = sheet[i].length; j < cols; j++) {
 				char val = sheet[i][j];
-				if( val == USAGE_CONFLICT )
+				if (val == USAGE_CONFLICT)
 					populatedCells++;
 			}
 		}
-		
+
 		return populatedCells;
 	}
-	
+
 	public static void printFabricLayout(char[][] rows) {
 		StringBuilder so = new StringBuilder();
 
@@ -82,5 +106,28 @@ public class AdventDay03 {
 		}
 
 		System.out.println(so.toString());
+	}
+
+	private static Collection<FabricClaim> parseFabricClaims(Collection<String> claims) {
+		Collection<FabricClaim> convertedClaims = new ArrayList<>();
+		for (String claim : claims) {
+			convertedClaims.add(FabricClaim.parseClaim(claim));
+		}
+
+		return convertedClaims;
+	}
+
+	private static Collection<String> getDataFromInputFile() throws IOException {
+		try {
+			// the input file's stored relative to the class,
+			// so retrieve it w/ the classloader
+			ClassLoader classLoader = AdventDay01.class.getClassLoader();
+			Path p = Paths.get(classLoader.getResource("fabric-usage.txt").toURI());
+			// Files.lines stealthily opens (and keeps open) a handle, so
+			// we need to ensure that the stream it produces gets closed
+			return Files.readAllLines(p);
+		} catch (URISyntaxException urie) {
+			throw new IOException("Unable to load input files, check file layout.");
+		}
 	}
 }
