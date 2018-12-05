b985c835de67e10dea0b9d7811c5ded597feebd6
Owen Searfoss <owen@datura.io>
2018-12-04 23:05:32 -0600

    Handle events, added test cases

--- src/main/java/io/datura/java/quizzes/advent2018/day04/AdventDay04.java
+++ src/main/java/io/datura/java/quizzes/advent2018/day04/AdventDay04.java
@@ -1,5 +1,111 @@
 package io.datura.java.quizzes.advent2018.day04;
 
-public class AdventDay04 {
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class AdventDay04 {
+	private enum GuardState {
+		ARRIVED, FELL_ASLEEP, AWOKE, UNKNOWN
+	};
+
+	public static void main(String[] args) {
+		try {
+			List<String> activities = loadIntelFile();
+			evalGuardSleepTimes(activities);
+		} catch (IOException ioe) {
+			System.err.println("Encountered an error when handling input file, aborting.");
+			System.exit(1);
+		}
+	}
+
+	public static Map<Integer, Long> evalGuardSleepTimes(List<String> activities) {
+		Integer guard = null;
+		GuardState guardState = GuardState.UNKNOWN;
+		LocalDateTime lastEvent = null;
+
+		Map<Integer, Long> sleepTimesByGuard = new HashMap<>();
+
+		for (String activity : activities) {
+			LocalDateTime tmpTime = SortIntelFile.parseInputDate(activity);
+
+			guardState = getStateFromActivity(activity);
+			switch (guardState) {
+			case ARRIVED:
+				// a new guard's on duty, so reset the clock and the id
+				guard = getCurrentGuardFromActivity(activity);
+				lastEvent = tmpTime;
+				break;
+			case FELL_ASLEEP:
+				lastEvent = tmpTime;
+				break;
+			case AWOKE:
+				// the guard had fallen asleep, so determine how long they'd been sleeping
+				Long slept = getTimeAsleep(lastEvent, tmpTime);
+
+				// make a record of time spent asleep
+				if (sleepTimesByGuard.containsKey(guard)) {
+					Long timeSlept = sleepTimesByGuard.get(guard);
+					timeSlept += slept;
+					sleepTimesByGuard.put(guard, timeSlept);
+				} else {
+					sleepTimesByGuard.put(guard, slept);
+				}
+
+				// reset the clock
+				lastEvent = tmpTime;
+				break;
+			case UNKNOWN:
+			default:
+				System.err.println("Unknown state change for line: " + activity);
+				break;
+			}
+		}
+		
+		return Collections.unmodifiableMap(sleepTimesByGuard);
+	}
+
+	public static Long getTimeAsleep(LocalDateTime ts, LocalDateTime ta) {
+		return ChronoUnit.MINUTES.between(ts, ta);
+	}
+
+	public static Integer getCurrentGuardFromActivity(String activity) {
+		int guardIdStart = activity.indexOf('#') + 1;
+		String guardId = activity.substring(guardIdStart, activity.indexOf(' ', guardIdStart));
+		try {
+			return Integer.parseInt(guardId);
+		} catch (NumberFormatException nfe) {
+			nfe.printStackTrace();
+			return 0;
+		}
+	}
+
+	public static GuardState getStateFromActivity(String activity) {
+		if (activity.endsWith("begins shift"))
+			return GuardState.ARRIVED;
+		else if (activity.endsWith("falls asleep"))
+			return GuardState.FELL_ASLEEP;
+		else if (activity.endsWith("wakes up"))
+			return GuardState.AWOKE;
+		else
+			return GuardState.UNKNOWN;
+	}
+
+	private static List<String> loadIntelFile() throws IOException {
+		try {
+			ClassLoader cl = SortIntelFile.class.getClassLoader();
+			Path input = Paths.get(cl.getResource("intel-sorted.txt").toURI());
+			return Files.readAllLines(input);
+		} catch (URISyntaxException urie) {
+			throw new IOException("Encountered an error when handling intel file.");
+		}
+	}
 }
--- src/test/java/io/datura/java/quizzes/advent2018/AdventDay04Test.java
+++ src/test/java/io/datura/java/quizzes/advent2018/AdventDay04Test.java
@@ -1,16 +1,19 @@
 package io.datura.java.quizzes.advent2018;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Test;
 
+import io.datura.java.quizzes.advent2018.day04.AdventDay04;
 import io.datura.java.quizzes.advent2018.day04.GuardIntelDateComparator;
 import io.datura.java.quizzes.advent2018.day04.SortIntelFile;
 
@@ -42,5 +45,50 @@ public class AdventDay04Test {
 		assertEquals(1518, dateTime.getYear());
 		assertEquals(10, dateTime.getMonthValue());
 		assertEquals(31, dateTime.getDayOfMonth());
+	}
+
+	@Test
+	public void testGetGuardId() {
+		String activity = "[1518-03-28 23:59] Guard #1777 begins shift";
+		int guardId = AdventDay04.getCurrentGuardFromActivity(activity);
+		assertEquals(1777, guardId);
+	}
+
+	@Test
+	public void testGuardSleepTime() {
+		LocalDateTime fellAsleep = LocalDateTime.of(1518, 11, 1, 0, 5);
+		LocalDateTime awoke = LocalDateTime.of(1518, 11, 1, 0, 25);
+		long minsAsleep = AdventDay04.getTimeAsleep(fellAsleep, awoke);
+		assertEquals(20l, minsAsleep);
+	}
+
+	@Test
+	public void testGuardSleepTime2() {
+		LocalDateTime fellAsleep = LocalDateTime.of(1518, 03, 27, 0, 16);
+		LocalDateTime awoke = LocalDateTime.of(1518, 03, 27, 0, 33);
+		long minsAsleep = AdventDay04.getTimeAsleep(fellAsleep, awoke);
+		assertEquals(17l, minsAsleep);
+	}
+	
+	@Test
+	public void testSleepTimeCalculation() {
+		String[] activities = new String[] {
+				"[1518-03-27 00:03] Guard #2251 begins shift", "[1518-03-27 00:11] falls asleep",
+				"[1518-03-27 00:57] wakes up", "[1518-03-27 23:58] Guard #3319 begins shift",
+				"[1518-03-28 00:16] falls asleep", "[1518-03-28 00:33] wakes up", "[1518-03-28 00:53] falls asleep" };
+		
+		List<String> events = Arrays.asList(activities);
+		Map<Integer, Long> sleepTimes = AdventDay04.evalGuardSleepTimes(events);
+		
+		// should consist of two guards
+		assertEquals(2, sleepTimes.keySet().size());
+		// guard 2251
+		assertTrue(sleepTimes.containsKey(Integer.valueOf(2251)));
+		// guard 2251 slept for 46 minutes
+		assertEquals(Long.valueOf(46l), sleepTimes.get(2251));
+		// guard 3319
+		assertTrue(sleepTimes.containsKey(Integer.valueOf(3319)));
+		// guard 3319 slept for 17 minutes
+		assertEquals(Long.valueOf(17l), sleepTimes.get(3319));
 	}
 }
