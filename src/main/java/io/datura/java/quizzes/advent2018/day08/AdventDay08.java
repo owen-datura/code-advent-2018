eb1499779046537c6021f2e51c551e821e4bed7b
Owen Searfoss <owen@datura.io>
2018-12-08 03:24:52 -0600

    Created a wrapper, solved part one.

---
+++ src/main/java/io/datura/java/quizzes/advent2018/day08/AdventDay08.java
@@ -0,0 +1,35 @@
+package io.datura.java.quizzes.advent2018.day08;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class AdventDay08 {
+	public static void main(String[] args) {
+		try {
+			String nodeList = loadNodeFile();
+			Node n = LicenseReader.createNodeFromLicenseString(nodeList);
+			int sum = LicenseReader.sumMetaData(n);
+			String output = String.format("Summed nodes, total was %d.", sum);
+			System.out.println(output);
+		} catch (IOException e) {
+			System.exit(1);
+		}
+	}
+
+	private static String loadNodeFile() throws IOException {
+		try {
+			ClassLoader cl = AdventDay08.class.getClassLoader();
+			Path p = Paths.get(cl.getResource("nodes.txt").toURI());
+			try (BufferedReader reader = Files.newBufferedReader(p, StandardCharsets.UTF_8)) {
+				return reader.readLine();
+			}
+		} catch (URISyntaxException urie) {
+			throw new IOException("Encountered an I/O exception when reading input file.");
+		}
+	}
+}
