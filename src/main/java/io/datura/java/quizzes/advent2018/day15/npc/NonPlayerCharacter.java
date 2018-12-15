1cfe8b564c2d2435859c495c53ac479996a9bdcf
Owen Searfoss <owen@datura.io>
2018-12-15 03:14:47 -0600

    added npc attributes

--- src/main/java/io/datura/java/quizzes/advent2018/day15/npc/NonPlayerCharacter.java
+++ src/main/java/io/datura/java/quizzes/advent2018/day15/npc/NonPlayerCharacter.java
@@ -4,12 +4,26 @@ import io.datura.java.quizzes.advent2018.day15.GameEntity;
 
 public abstract class NonPlayerCharacter implements GameEntity {
 	int orderOfAttack;
+	int hitPoints = 300;
+	int attackPower = 3;
 
 	public int getOrderOfAttack() {
 		return orderOfAttack;
 	}
-	
+
 	public void setOrderOfAttack(int orderOfAttack) {
 		this.orderOfAttack = orderOfAttack;
+	}
+
+	public int getHitPoints() {
+		return hitPoints;
+	}
+
+	public void doDamage(int minusHP) {
+		this.hitPoints = Math.max((this.hitPoints - minusHP), 0);
+	}
+
+	public int getAttackPower() {
+		return attackPower;
 	}
 }
